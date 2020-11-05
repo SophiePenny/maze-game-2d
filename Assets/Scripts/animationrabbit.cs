99be6eef119b58cfcfcfd3c9084017854bac658b
SophiePenny <512894@chrome.escg.ac.uk>
2020-11-05 16:08:15 +0000

    this is actually functional now

---
+++ Assets/Scripts/animationrabbit.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animationrabbit : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;
+    public Sprite bunnysheet5_1;
+    public Sprite bunnysheet5_10;
+    public Sprite bunnysheet5_21;
+    public Sprite bunnysheet5_31;
+    public Sprite bunnysheet5_3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = bunnysheet5_1;
+
+        if(Input.GetKey("w"))
+        {
+            spriteRenderer = GetComponent<SpriteRenderer>();
+            spriteRenderer.sprite = bunnysheet5_10;
+        }
+
+        if(Input.GetKey("d"))
+        {
+            spriteRenderer = GetComponent<SpriteRenderer>();
+            spriteRenderer.sprite = bunnysheet5_21;
+        }
+
+        if(Input.GetKey("a"))
+        {
+            spriteRenderer = GetComponent<SpriteRenderer>();
+            spriteRenderer.sprite = bunnysheet5_31;
+        }
+
+        if(Input.GetKey("s"))
+        {
+            spriteRenderer = GetComponent<SpriteRenderer>();
+            spriteRenderer.sprite = bunnysheet5_3;
+        }
+
+    }
+}
---
+++ Assets/Scripts/rabbitmove.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rabbitmove : MonoBehaviour
+{
+    Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 Velocity = rb.velocity;
+        if (Input.GetKey("a"))
+        {
+            Velocity.x = -1;
+        }
+
+        if (Input.GetKey("d"))
+        {
+            Velocity.x = 1;
+        }
+
+        if (Input.GetKey("w"))
+        {
+            Velocity.y = 1;
+        }
+
+        if(Input.GetKey("s"))
+        {
+            Velocity.y = -1;
+        }
+        rb.velocity = Velocity;
+    }
+}
