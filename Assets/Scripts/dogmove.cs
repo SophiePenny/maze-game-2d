6f64b442b59ea824e2f5881bb3ee0745d13811dc
SophiePenny <512894@chrome.escg.ac.uk>
2020-11-06 17:02:37 +0000

    working on the enemy

---
+++ Assets/Scripts/dogmove.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dogmove : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;
+    public Sprite Dog1_0;
+    public Sprite Dog_1_1_0;
+    Rigidbody2D rb;
+    int timer;
+    int direction;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = 0;
+        direction = -10;
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = Dog1_0;
+        timer ++;
+        //Debug.Log(timer);
+        rb.velocity = new Vector2(direction, 0);
+        if (timer > 700)
+        {
+            direction = -direction;
+            spriteRenderer = GetComponent<SpriteRenderer>();
+            spriteRenderer.sprite = Dog_1_1_0;
+            timer = 0;
+        }
+    }
+}
